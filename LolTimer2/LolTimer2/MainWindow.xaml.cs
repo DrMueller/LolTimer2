00ad2fc834e92d068a0e88399623464566c35de3
Müller Matthias <matthias.mueller@noser.com>
2023-12-12 18:13:42 +0100

    init

---
+++ LolTimer2/LolTimer2/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using LolTimer2.Models;
+
+namespace LolTimer2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public FlashTimer TopTimer { get; set; }
+        public FlashTimer JungleTimer { get; set; }
+        public FlashTimer MidTimer { get; set; }
+        public FlashTimer AdTimer { get; set; }
+        public FlashTimer SupportTimer { get; set; }
+
+        public MainWindow()
+        {
+            TopTimer = new FlashTimer("Top");
+            JungleTimer = new FlashTimer("Jung");
+            MidTimer = new FlashTimer("Mid");
+            AdTimer = new FlashTimer("Ad");
+            SupportTimer = new FlashTimer("Sup");
+            
+            this.DataContext = this;
+            InitializeComponent();
+        }
+
+        private void TopTimer_Tick(object? sender, EventArgs e)
+        {
+        }
+
+        private void TopClicked(object sender, RoutedEventArgs e)
+        {
+            TopTimer.StartTimer();
+        }
+
+
+        private void MidClicked(object sender, RoutedEventArgs e)
+        {
+            MidTimer.StartTimer();
+        }
+
+        private void SupportClicked(object sender, RoutedEventArgs e)
+        {
+            SupportTimer.StartTimer();
+        }
+
+        private void AdClicked(object sender, RoutedEventArgs e)
+        {
+            AdTimer.StartTimer();
+        }
+
+        private void JungleClicked(object sender, RoutedEventArgs e)
+        {
+            JungleTimer.StartTimer();
+        }
+    }
+}
---
+++ LolTimer2/LolTimer2/Models/FlashTimer.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace LolTimer2.Models;
+
+public class FlashTimer : INotifyPropertyChanged
+{
+    private readonly DispatcherTimer _timer;
+    private int _flashUpInSeconds;
+
+    public FlashTimer(string roleDescription)
+    {
+        RoleDescription = roleDescription;
+        _timer = new DispatcherTimer();
+        _timer.Interval = TimeSpan.FromSeconds(1);
+        _timer.Tick += _timer_Tick;
+    }
+
+    public string RoleDescription { get; }
+
+    public string TimerDescription
+    {
+        get
+        {
+            if (_flashUpInSeconds == 0) return "UP";
+
+            var ts = TimeSpan.FromSeconds(_flashUpInSeconds);
+            return ts.ToString(@"mm\:ss");
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void _timer_Tick(object? sender, EventArgs e)
+    {
+        if (_flashUpInSeconds <= 0) _timer.Stop();
+
+        _flashUpInSeconds--;
+        OnPropertyChanged(nameof(TimerDescription));
+    }
+
+    public void StartTimer()
+    {
+        _flashUpInSeconds = 300;
+        _timer.Start();
+    }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
